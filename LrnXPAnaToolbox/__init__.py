d86ad376f93d37789889f7aafc4b25859e2c6507
Kayané <kayane-r@evidenceb.com>
2020-09-01 18:31:32 +0200

    Bump version: 0.3.4 → 0.3.5

--- LrnXPAnaToolbox/__init__.py
+++ LrnXPAnaToolbox/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kayané Elmayan Robach"""
 __email__ = 'kaya.robach@gmail.com'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 from .lib import str_question_tolist
 from .lib import list_question_tostr
