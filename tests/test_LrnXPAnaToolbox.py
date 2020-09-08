4afaece00c07305849a2c3f925c49783d42082e0
ylsz <le-ul@hotmail.fr>
2020-09-09 01:39:35 +0200

    [Add] TU of recommendation and data file with a .csv test

--- tests/test_LrnXPAnaToolbox.py
+++ tests/test_LrnXPAnaToolbox.py
@@ -7,6 +7,7 @@ import sklearn
 from sklearn import preprocessing
 import pandas as pds
 import os
+import datetime
 import LrnXPAnaToolbox as lrn
 from LrnXPAnaToolbox import *
 
@@ -38,6 +39,40 @@ class Test_cluster_students(unittest.TestCase):
         new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
         new_df = lrn.similar_students(df, new_stud, False)
         new_df = pds.DataFrame(new_df[0])
+        today = datetime.date.today()
+        os.remove((os.path.join(".","data", f"DF_MBKMeans_{today}.pk1")))
         if {'id_eleve', 'Cluster'}.issubset(new_df.columns):
             count += 1
         self.assertEqual(count, 1)
+
+class Test_recommender(unittest.TestCase):
+
+    def test_dropout(self):
+        count = 0
+        df = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
+        new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
+        recom = pds.read_csv((os.path.join(".", "data", "recom.csv")))
+        recom['question_id'] = recom['question_id'].astype(str)
+        after_dropout = lrn.dropout_recommendation(new_stud, df, recom)
+        after_dropout = pds.DataFrame(after_dropout)
+        if {'weighted_average_recom_score', 'question_id'}.issubset(after_dropout.columns):
+            count += 1
+        self.assertEqual(count, 1)
+
+    def test_recom_algorithm(self):
+        marks_df = pds.read_pickle((os.path.join("..", "..", "data", "table_marks_2020-08-26.pk1")))
+        new_stud_marks = pds.read_pickle((os.path.join("..", "..", "data", "new_student_marks_2020-08-26.pk1")))
+        marks_df.columns = ['student_id', 'question_id', 'rating']
+        new_stud_marks.columns = ['student_id', 'question_id', 'rating']
+        recom_df = lrn.recom_algorithm(new_stud_marks, marks_df)
+        self.assertEqual(20, len(recom_df))
+
+    def test_recom_after_dropout(self):
+        df = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
+        new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
+        marks_df = pds.read_pickle((os.path.join("..", "..", "data", "table_marks_2020-08-26.pk1")))
+        new_stud_marks = pds.read_pickle((os.path.join("..", "..", "data", "new_student_marks_2020-08-26.pk1")))
+        marks_df.columns = ['student_id', 'question_id', 'rating']
+        new_stud_marks.columns = ['student_id', 'question_id', 'rating']
+        quest_after_dropout = lrn.algorithm(new_stud, new_stud_marks, df, marks_df)
+        self.assertGreaterEqual(len(quest_after_dropout), 1)
