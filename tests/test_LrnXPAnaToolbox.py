5190d2f7c4223c42991b57f26c6b8aa8f0480303
UQD <le-ul@hotmail.fr>
2020-09-12 17:01:01 +0200

    [Add] UT for marks creation

--- tests/test_LrnXPAnaToolbox.py
+++ tests/test_LrnXPAnaToolbox.py
@@ -15,16 +15,16 @@ class Test_cluster_students(unittest.TestCase):
 
     def test_creation_df(self):
         count = 0
-        pickle = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
-        data = lrn.cluster_students.cluster_students.creationDFClustering(pickle)
+        csv = pds.read_csv((os.path.join(".", "data", "stud_df_100.csv")))
+        data = lrn.cluster_students.cluster_students.creationDFClustering(csv)
         if {'id_eleve','temps_moyen_reponse','taux_reussite','parcours',
         'duree_totale','nbr_questions','jour','horaire'}.issubset(data.columns):
             count += 1
         self.assertEqual(1, count)
 
     def test_optimal_nbr_cluster(self):
-        pickle = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
-        data = lrn.creationDFClustering(pickle)
+        csv = pds.read_csv((os.path.join(".", "data", "stud_df_100.csv")))
+        data = lrn.creationDFClustering(csv)
         length = len(data['id_eleve'].unique())
         DFVariables = data.drop(['id_eleve'], axis = 1)
         DFVariables = DFVariables.apply(pds.to_numeric)
@@ -35,8 +35,8 @@ class Test_cluster_students(unittest.TestCase):
 
     def test_similar_students(self):
         count = 0
-        df = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
-        new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
+        df = pds.read_csv((os.path.join(".", "data", "stud_df_100.csv")))
+        new_stud = pds.read_csv((os.path.join(".", "data", "new_stud_test.csv")))
         new_df = lrn.similar_students(df, new_stud, False)
         new_df = pds.DataFrame(new_df[0])
         today = datetime.date.today()
@@ -49,8 +49,8 @@ class Test_recommender(unittest.TestCase):
 
     def test_dropout(self):
         count = 0
-        df = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
-        new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
+        df = pds.read_csv((os.path.join(".", "data", "stud_df_100.csv")))
+        new_stud = pds.read_csv((os.path.join(".", "data", "new_stud_test.csv")))
         recom = pds.read_csv((os.path.join(".", "data", "recom.csv")))
         recom['question_id'] = recom['question_id'].astype(str)
         after_dropout = lrn.dropout_recommendation(new_stud, df, recom)
@@ -60,19 +60,17 @@ class Test_recommender(unittest.TestCase):
         self.assertEqual(count, 1)
 
     def test_recom_algorithm(self):
-        marks_df = pds.read_pickle((os.path.join("..", "..", "data", "table_marks_2020-08-26.pk1")))
-        new_stud_marks = pds.read_pickle((os.path.join("..", "..", "data", "new_student_marks_2020-08-26.pk1")))
-        marks_df.columns = ['student_id', 'question_id', 'rating']
-        new_stud_marks.columns = ['student_id', 'question_id', 'rating']
+        marks_df = pds.read_csv((os.path.join(".", "data", "stud_marks_100.csv")))
+        new_stud_marks = pds.read_csv((os.path.join(".", "data", "new_stud_marks_test.csv")))
         recom_df = lrn.recom_algorithm(new_stud_marks, marks_df)
         self.assertEqual(20, len(recom_df))
 
-    def test_recom_after_dropout(self):
-        df = pds.read_pickle((os.path.join("..", "..", "data", "chatbot_dataset_2020-08-26.pk1")))
-        new_stud = pds.read_pickle((os.path.join("..", "..", "data", "new_student_dataset_2020-08-26.pk1")))
-        marks_df = pds.read_pickle((os.path.join("..", "..", "data", "table_marks_2020-08-26.pk1")))
-        new_stud_marks = pds.read_pickle((os.path.join("..", "..", "data", "new_student_marks_2020-08-26.pk1")))
-        marks_df.columns = ['student_id', 'question_id', 'rating']
-        new_stud_marks.columns = ['student_id', 'question_id', 'rating']
-        quest_after_dropout = lrn.algorithm(new_stud, new_stud_marks, df, marks_df)
-        self.assertGreaterEqual(len(quest_after_dropout), 1)
+class Test_marks_creator(unittest.TestCase):
+
+    def test_marks_table(self):
+        count = 0
+        df = pds.read_csv((os.path.join(".", "data", "stud_df_100.csv")))
+        marks = lrn.marks_table(df)
+        if {'student', 'question', 'mark'}.issubset(marks.columns):
+            count += 1
+        self.assertEqual(1, count)
